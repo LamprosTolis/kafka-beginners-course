2c6361120c259d4f008368442eadbaca4af8affb
Lampros Tolis <37981510+LamprosTolis@users.noreply.github.com>
2020-12-15 10:14:26 +0200

    Refactored the code Added dependencies for elastic search

---
+++ kafka-consumer-elasticsearch/src/main/java/com/gihub/LamprosTolis/kafka/tutorial3/ElasticSearchConsumer.java
@@ -0,0 +1,7 @@
+package com.gihub.LamprosTolis.kafka.tutorial3;
+
+public class ElasticSearchConsumer {
+    public static void main(String[] args) {
+
+    }
+}
--- kafka-producer-twitter/src/main/java/tutorial2/TwitterProducer.java
+++ kafka-producer-twitter/src/main/java/tutorial2/TwitterProducer.java
@@ -1,4 +1,4 @@
-package com.github.com.LamprosTolis.kafka.tutorial2;
+package tutorial2;
 
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
